5cc5e99f9014d070308af9961b306e45504888f3
Raf98 <rafaelrutzsantos@gmail.com>
2020-10-23 03:27:15 -0300

    Finished StackMachine

--- JavaCompiler/MP/MaquinaDePilha.java
+++ JavaCompiler/MP/MaquinaDePilha.java
@@ -1,23 +1,14 @@
 import java.util.Queue;
 import java.util.Stack;
 import java.util.LinkedList;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.*;
 
 public class MaquinaDePilha {
 
     Stack<Integer> numbersStack;
     Queue<String> instructions;
-
-    public Stack<Integer> getNumbersStack() {
-        return this.numbersStack;
-    }
-
-    public Queue<String> getInstructions() {
-        return this.instructions;
-    }
 
     int processOps() {
         return 0;
@@ -29,19 +20,60 @@ public class MaquinaDePilha {
     }
 
     public static void main(String[] args) {
-        Stream<String> stream;
         MaquinaDePilha mp = new MaquinaDePilha();
 
         try {
-            stream = Files.lines(Paths.get(args[0]));
-            Object[] strs = stream.toArray();
-            System.out.println(strs[0]);
-            stream.forEach(line -> mp.getInstructions().add(line));
+            File inputFile = new File(args[0]);
+            Scanner fileReader = new Scanner(inputFile);
+
+            while (fileReader.hasNextLine()) {
+                mp.instructions.add(fileReader.nextLine());
+            }
+
+            fileReader.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
 
-        mp.getInstructions().forEach(System.out::println);
-        System.out.println(mp.getInstructions().peek()); 
+        // mp.instructions.forEach(System.out::println);
+
+        Pattern operations = Pattern.compile("SUM|MULT|SUB|DIV");
+
+        mp.instructions.forEach(instruction -> {
+            if (instruction.contains("PUSH")) {
+                mp.numbersStack.add(Integer.parseInt(instruction.substring(5)));
+            } else if (operations.matcher(instruction).matches()) {
+                //System.out.println(instruction);
+                if (mp.numbersStack.size() >= 2) {
+                    //mp.numbersStack.forEach(System.out::println);
+                    int num0 = mp.numbersStack.pop();
+                    int num1 = mp.numbersStack.pop();
+
+                    switch (instruction) {
+                        case "SUM":
+                            mp.numbersStack.add(num1 + num0);
+                            break;
+                        case "MULT":
+                            mp.numbersStack.add(num1 * num0);
+                            break;
+                        case "SUB":
+                            mp.numbersStack.add(num1 - num0);
+                            break;
+                        case "DIV":
+                            mp.numbersStack.add(num1 / num0);
+                            break;
+                    }
+                } else {
+                    System.out.println("Erro! Máquina de Pilha não conseguiu processar o programa.");
+                }
+            } else if (instruction.equals("PRINT")) {
+                System.out.println(mp.numbersStack.pop());
+            } else {
+                System.out.println("Erro! Máquina de Pilha não reconhece essa instrução!");
+            }
+        });
+
+        // mp.numbersStack.forEach(System.out::println);
+
     }
 }
